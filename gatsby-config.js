aae3b49ba8e293b8bb36028f4509d5137d852dc0
Garrit Franke <garrit@slashdev.space>
2022-03-16 19:47:38 +0100

    feat: init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,44 @@
+require("dotenv").config();
+
 /** @type {import('gatsby').GatsbyConfig} */
 module.exports = {
-  siteMetadata: {
-      title: ``,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    siteMetadata: {
+        title: ``,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "pages"
-  }]
+    plugins: [
+        "gatsby-plugin-sass",
+        "gatsby-plugin-mdx",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/",
+            },
+            __key: "pages",
+        },
+        {
+            resolve: `gatsby-source-github-api`,
+            options: {
+                // token: required by the GitHub API
+                token: process.env.GITHUB_TOKEN,
+                graphQLQuery: `query {
+                  repository(name: "seeking-maintainers", owner: "garritfra") {
+                    id
+                    issues(first: 100, labels: "seeking-maintainers", states: OPEN) {
+                      edges {
+                        node {
+                          id
+                          body
+                          title
+                        }
+                      }
+                    }
+                  }
+                }`,
+
+                variables: {},
+            },
+        },
+    ],
 };
