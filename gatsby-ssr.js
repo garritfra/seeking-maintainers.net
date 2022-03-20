e9a533602aee12343c8427fb010e262343740f87
Garrit Franke <garrit@slashdev.space>
2022-03-20 20:12:14 +0100

    seo: add title and description

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,5 +7,11 @@ export function onRenderBody({ setHeadComponents }) {
             data-domain="seeking-maintainers.net"
             src="https://analytics.slashdev.space/js/plausible.js"
         ></script>,
+        <title key="title">Seeking Maintainers</title>,
+        <meta
+            name="description"
+            content="The Seeking Maintainers Index"
+            key="description"
+        />,
     ]);
 }
